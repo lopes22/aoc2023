b05cdd9d00b3e4033662921e2c718f907c16b57b
lopes22 <lopes92290@gmail.com>
2023-12-09 20:18:34 -0500

    day9

---
+++ day9_part_2.py
@@ -0,0 +1,30 @@
+
+file = open('day9.txt', 'r')
+lines = file.readlines()
+
+rows = []
+
+for line in lines:
+    rows.append([int(x) for x in line.strip().split(' ')])
+
+total = 0
+for r in rows:
+    histories = [r]
+    cur_sequence = r
+
+    while not all(x == 0 for x in cur_sequence):
+        new_sequence = []
+        for i in range(1, len(cur_sequence)):
+            new_sequence.append(cur_sequence[i] - cur_sequence[i - 1])
+
+        histories.append(new_sequence)
+        cur_sequence = new_sequence 
+    
+    cur_element = 0
+    for hi in range(len(histories) - 2, -1, -1):
+        cur_element = histories[hi][0] - cur_element
+    
+    total += cur_element
+
+print(total)
+
